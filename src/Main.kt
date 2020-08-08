e67300b82c266d1bb60ee01b7bc8307c42e0dea1
muhammadisa <hyperspace018@gmail.com>
2020-08-08 21:52:58 +0700

    add staircase left and right

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,34 @@
 import java.util.*
 
+fun staircaseLeft(stair: Int){
+    var str = "#"
+    for (i in 0 until stair) {
+        println(str)
+        str += "#"
+    }
+}
 
+fun staircaseRight(stair: Int) {
+    var str = "#"
+    for (i in 0 until stair) {
+        System.out.printf("%"+stair+"s\n", str)
+        str += "#"
+    }
+}
 
-fun fizzBuzz(start: Int, end: Int){
+fun miniMaxSum(arr: Array<Long>) {
+    var intPool: MutableList<Long> = arr.toMutableList()
+    val intPoolReversed: MutableList<Long> = arr.reversed().toMutableList()
+    val result: ArrayList<Long> = arrayListOf()
+    intPoolReversed.forEach { i ->
+        intPool.remove(i)
+        result.add(intPool.sum())
+        intPool = arr.toMutableList()
+    }
+    println("${result.min()}, ${result.max()}")
+}
+
+fun fizzBuzz(start: Int, end: Int) {
     println((start..end).map { i ->
         mapOf(
             0 to i,
@@ -52,4 +78,7 @@ fun main(args: Array<String>) {
     isPalindrome("abba")
     isAnagram("listen", "silent")
     findDuplicate(arrayOf(1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10))
+    miniMaxSum(arrayOf(793810624, 895642170, 685903712, 623789054, 468592370))
+    staircaseLeft(8)
+    staircaseRight(8)
 }
